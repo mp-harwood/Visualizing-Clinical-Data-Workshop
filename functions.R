705e4594207040121924ff5db372540674b128d4
mp-harwood <133031701+mp-harwood@users.noreply.github.com>
2026-09-23 10:22:54 -0400

    Update functions.R

--- functions.R
+++ functions.R
@@ -70,7 +70,7 @@ create_meanplot <- function(param = "EOS", treatments = all_treatments) {
     scale_color_manual(values = trt_colors[treatments]) +
     scale_x_continuous(breaks = xticks, expand = c(0, 0)) +
     coord_cartesian(xlim = c(0, max(xticks) + 0.5), clip = "off") +
-    theme_classic(base_size = 16) +
+    theme_classic(base_size = 10) +
     theme(axis.title = element_text(face = "bold"), legend.position = "none")
 }
 
@@ -120,7 +120,7 @@ create_box_plot <- function(param = "ALB",treatments = all_treatments, crosstalk
     facet_wrap(~ AVISIT, nrow = 1) +
     labs(x = NULL, y = param_label[1]) +
     scale_color_manual(values = trt_colors[treatments]) +
-    theme_classic(base_size = 16) +
+    theme_classic(base_size = 10) +
     theme(
       axis.text.x      = element_text(angle = 45, hjust = 1),
       axis.title       = element_text(face = "bold"),
